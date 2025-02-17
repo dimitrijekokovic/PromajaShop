bc22a796280c9c6e61b68fb2b66dcfcd56fed874
dimitrijekokovic <dimitrije.kokovic@prvabeogim.edu.rs>
2025-02-17 13:27:27 +0100

    Pojavljivanje proizvoda u admin panel

--- .history/pages/products_20241206182602.js
+++ .history/pages/products_20241206182602.js
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 export default function Products() {
     const [products,setProducts] = useState([]);
     useEffect(() => {
-        axios.get('/api/products').then(response => {
+        axios.get('http://localhost:3000/api/products').then(response => {
             setProducts(response.data);
         });
     }, []);
