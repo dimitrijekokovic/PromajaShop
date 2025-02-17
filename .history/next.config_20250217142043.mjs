d35751d88b6850f5292206a3e0ef1b18a8d5c079
dimitrijekokovic <dimitrije.kokovic@prvabeogim.edu.rs>
2025-02-17 14:21:36 +0100

    Popravljanje slika admin panel pt2

---
+++ .history/next.config_20250217142043.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Aktivirajte striktni mod za otkrivanje problema u razvoju
+    images: {
+      domains: ['lh3.googleusercontent.com'], // Dozvolite slike sa Google-a za profilne slike
+      ['dimitrije-next-ecommerce.s3.amazonaws.com']
+    },
+    
+    async redirects() {
+      return [
+        {
+          source: '/login',
+          destination: '/',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
